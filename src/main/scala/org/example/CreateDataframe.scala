4ef40c447172033a0d541ce971bea654d26c76db
Vennuvenkatesh <134073062+Vennuvenkatesh@users.noreply.github.com>
2023-05-20 22:50:43 +0530

    Update CreateDataframe.scala
    we can take vertical = true as arguement in show

--- src/main/scala/org/example/CreateDataframe.scala
+++ src/main/scala/org/example/CreateDataframe.scala
@@ -7,7 +7,7 @@ object CreateDataframe {
     val spark = SparkSession.builder().master("local[*]").appName("CreateDataframe").getOrCreate()
     import spark.implicits._
     val data = Seq(("hadoop", "3000")).toDF("course", "fee")
-    data.show(1000, false)
+    data.show(1000, 200, true)
     data.printSchema()
   }
 
