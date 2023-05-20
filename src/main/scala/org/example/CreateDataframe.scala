20c1fb9b0dc357e908f1a5cd1f05ee3ab55c1d22
vennu <vennuvenkatesh520@gmail.com>
2023-05-20 22:55:00 +0530

    changed variable name from data to df

--- src/main/scala/org/example/CreateDataframe.scala
+++ src/main/scala/org/example/CreateDataframe.scala
@@ -6,9 +6,9 @@ object CreateDataframe {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().master("local[*]").appName("CreateDataframe").getOrCreate()
     import spark.implicits._
-    val data = Seq(("hadoop", "3000")).toDF("course", "fee")
-    data.show(1000, 200, true)
-    data.printSchema()
+    val df = Seq(("hadoop", "3000")).toDF("course", "fee")
+    df.show(1000, 200, true)
+    df.printSchema()
   }
 
 }
