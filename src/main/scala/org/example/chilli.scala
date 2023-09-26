7c1ad146bfb841a665825787a91fdb809a9071ae
Vennuvenkatesh <134073062+Vennuvenkatesh@users.noreply.github.com>
2023-09-26 19:14:36 +0530

    Update chilli.scala

--- src/main/scala/org/example/chilli.scala
+++ src/main/scala/org/example/chilli.scala
@@ -9,7 +9,7 @@ object chilli {
     val df = Seq((1,"Teja",25000),(2,"Komali",26000),(3,"Bullet",22000),(4,"Bangaram",21000),(5,"Byadagi",32000),(6,"yello",32000))
 
         .toDF("id","Type","Cost")
-        //df.show(false)
+        df.show(false)
   }
 
 }
